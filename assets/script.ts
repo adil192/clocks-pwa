7e80e9e1ea44aaf4aee8b6f9aca16268c9b259b2
Adil Hanney <adilh+github@orchid.anonaddy.com>
2021-11-29 00:43:23 +0000

    clock: keep clock in view when window is resized

--- assets/script.ts
+++ assets/script.ts
@@ -1,8 +1,9 @@
 let clock_container = document.getElementById("clock-container");
+let clock_current = clock_container.querySelector("clock.focus");
 
 let minute_scrolled = -1;
 
-function scrollToCurrentTime() {
+function findNewCurrentTime() {
 	let now = new Date();
 	let minute_current = now.getMinutes();
 	let hour_current = now.getHours() % 12;
@@ -10,15 +11,19 @@ function scrollToCurrentTime() {
 	if (minute_current == minute_scrolled) return;
 	minute_scrolled = minute_current;
 
-	let clock_current = clock_container.querySelector(".minute-"+minute_current+".hour-"+hour_current);
-
-	let currently_focused = clock_container.querySelector("clock.focus");
-	if (currently_focused != null) {
-		currently_focused.classList.remove("focus");
+	if (clock_current != null) {
+		clock_current.classList.remove("focus");
 	}
-	clock_current.classList.add("focus");
 
+	clock_current = clock_container.querySelector(".minute-"+minute_current+".hour-"+hour_current);
+	clock_current.classList.add("focus");
+	scrollToCurrentTime();
+}
+
+function scrollToCurrentTime() {
 	clock_current.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
 }
 
-setInterval(scrollToCurrentTime, 1000);
+setInterval(findNewCurrentTime, 1000);
+
+window.addEventListener("resize", scrollToCurrentTime);
