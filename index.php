671bc65f60d2ffd08e3d9427b5ce76bdffc67dab
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-04-17 18:09:27 +0100

    perf: use local files instead of cdn

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ for ($i = 1; $i <= 720; ++$i) {
 <?php } ?>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="../assets/ext/popper.2.11.5.min.js"></script>
 <script src="assets/script.js"></script>
 </body>
 </html>
